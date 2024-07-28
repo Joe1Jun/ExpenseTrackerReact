a66580c066d0611a5afe4c7107a451105012276e
Joe1Jun <junker91joe@gmail.com>
2024-07-28 13:49:22 +0300

    Add addIncome controller to handle income creation  Created  controller to handle the creation of new income entries.  Imported  model for creating new income entries in the database. Added validation checks to ensure all required fields are present and amount is a positive number.  Added a check to ensure amount is a number.  Created a new instance of the  with the provided request body data. Saved the new income entry to the database and returned a success message.  Logged the saved income to the console for debugging purposes.  Used Postman to create a test object and insert it into the schema

--- Backend/controllers/income.js
+++ Backend/controllers/income.js
@@ -1,3 +1,4 @@
+const IncomeSchema = require ("../models/IncomeModel")
 
 
 
@@ -7,7 +8,32 @@
 
 
 exports.addIncome = async (req, res) => {
-console.log(req.body)
+    const { title, amount, category, description, date } = req.body;
+    
+   
+        // Create a new instance of the Income model
+        const income = new IncomeSchema({
+            title,
+            amount,
+            category,
+            description,
+            date
+        });
 
-
-}
+    try {
+           //validations
+        if (!title  || !category || !description || !date ) {
+            return res.status(400).json({message: "All fields are required"})
+        }
+        if (amount <= 0 || !amount === 'number') {
+            return res.status(400).json({message: 'Amount must be a positive number'})
+        }
+        await income.save()
+        res.status(200).json({message: 'Income Added'})
+       } catch (error) {
+        
+       }
+        // Log the saved income and send a response
+        console.log(income);
+        
+};
