4235c72d32d385d242004375c255951a305de326
Joe1Jun <junker91joe@gmail.com>
2024-08-02 11:57:24 +0200

    Implement Form component for adding income with state management and context integration. Added form fields for title, amount, date, category, and description.

--- Frontend/expense-tracker/src/Components/Incomes/Incomes.jsx
+++ Frontend/expense-tracker/src/Components/Incomes/Incomes.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import styled from 'styled-components'
+import { useGlobalContext } from "../../context/globalContext";
 import { InnerLayout } from "../../styles/Layouts";
-
+import Form from '../Form/Form'
 
 function Income() {
+    const { addIncome }= useGlobalContext()
     return (
         
         <IncomeStyled>
             <InnerLayout>
                 <h1>Incomes</h1>
-                
+                <div className="income-content">
+                    <div className="form-container">
+                        <Form />
+                    </div>
+                    <div className="incomes"></div>
+                </div>
           </InnerLayout>
 
 
